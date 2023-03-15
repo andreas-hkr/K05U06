b2c5a6a30aabf729bac6223a6a53e3663eaec8db
Andreas Nilsson <andreas.nilsson@hkr.se>
2023-03-15 19:05:54 +0100

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,12 @@
+public class Main {
+    public static void main(String[] args) {
+        // Beräkna befolkningen år X
+        //   - Befolkningen år 2020 = 26000
+        //   - Födda = 0.7% av befolkningen vid årets start
+        //   - Avlidna = 0.6%  av befolkningen vid årets start
+        //   - Inflyttade = 300 varje år
+        //   - Utflyttade = 325 varje år
+
+
+    }
+}
